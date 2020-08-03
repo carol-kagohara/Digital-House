2902b868f6ea119eefebe9e3204ddb1fe8ff272b
carol-kagohara <carol.kagohara96@gmail.com>
2020-08-03 11:17:18 -0300

    Add files via upload

---
+++ Aula - 27_07_2020 Java/java.js
@@ -0,0 +1,27 @@
+var positivo = 1;
+var negativo = -1;
+var decimal = 0.1;
+var string = "Teste";
+var verdadeiro = true;
+
+console.log(positivo);
+console.log(negativo);
+console.log(decimal);
+console.log(string);
+console.log(verdadeiro);
+
+var nome = "Caroline";
+var sobrenome = "Kagohara"
+console.log(nome + " " + sobrenome);
+console.log("Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam");
+
+var valorNulo = null;
+var naoDefinido = undefined;
+console.log(valorNulo);
+console.log(naoDefinido);
+
+frutas = ["morango","laranja","manga","amora","açai"];
+console.log(frutas);
+
+var frase = "Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam";
+console.log(frase);
